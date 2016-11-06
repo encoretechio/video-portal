cdd7768a09a77bc51c052ac05357bdb25f05f2de
Janaka Chathuranga <bjchathuranga@gmail.com>
2016-11-06 19:27:49 +0530

    Change Travis

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Playlist } from '../models/playlist';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [DataService]
 })
 export class HomeComponent implements OnInit {
 
-  playlists:Playlist[] = [
-  { id: 11, name: 'Mr. Nice' , description:"Desc",videoList:[ {  id: 123, title: "Video1", description:"string"},
-                                                          {  id: 123, title: "Video3", description:"string"},
-                                                          {  id: 123, title: "Video2", description:"string"} ] },
-  { id: 12, name: 'Mr. Nice - 1' , description:"Desc",videoList:[] },
-  { id: 13, name: 'Mr. Nice - 2' , description:"Desc",videoList:[] }
-];
+  playlists:Playlist[];
 
-  constructor() { }
+  constructor(
+  private dataService:DataService) { }
 
   ngOnInit() {
+    this.dataService.getPlaylists().then(result =>
+      {
+        this.playlists = result;
+      });
   }
 
 }
