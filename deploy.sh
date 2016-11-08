a8ff32d019bc4238174ff3bd5b2bcae1f31eb048
Janaka <janakact@users.noreply.github.com>
2016-11-08 20:44:34 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,7 @@
+forever stopall
+cd ~/video-portal
 git pull
 cd backend
 
-npm install
-npm install --save swig
-npm install sails-mysql
-npm install sails-mongo
-
-# remove this and test
-#npm install --save sails-hook-jwt-auth
-
-npm install lodash
-npm install async
-npm install bcrypt
-npm install jsonwebtoken
-npm install sails-build-dictionary
-npm install sails-util
-
-forever start app.js
-echo "Started The Forever Server"
+chmod +x ./setupsailsproject.sh
+./setupsailsproject.sh
