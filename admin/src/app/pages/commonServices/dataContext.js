6e9103e387be124d46a3baca7455d7981ee454dc
chathuranga94 <chathuranga94@live.com>
2016-11-16 21:50:48 +0530

    GET POST datacontext edited

--- admin/src/app/pages/commonServices/dataContext.js
+++ admin/src/app/pages/commonServices/dataContext.js
@@ -4,15 +4,45 @@
     angular.module('commonServices')
       .service('dataContext',function($http,connectInfo){
 
-        this.getData = function(route,postData){
+        this.getData = function(route){
+            console.log("GET data");
             console.log(connectInfo.url + ":" + connectInfo.port + "/" + route);
 
             //Test method to POST data example.
             $http({
+                method  : 'GET',
+                url     : connectInfo.url + ":" + connectInfo.port + "/" + route,
+                //data    : postData,
+                headers : { 'Content-Type': 'application/json',
+                            'Authorization' : 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjIsImlhdCI6MTQ3OTIyMjM0OX0.WAi12LVaNRQAjck2CnLDKifM1DHrTBnz9jtf6HXCel4',
+                            //'Access-Control-Request-Headers' : '*'
+                          }
+               })
+                .success(function(data) {
+                  if (data.errors) {
+                    // Showing errors.
+                    //console.log(data.errors);
+                  } else {
+                    console.log(data);
+                  }
+            });
+
+            return 200;
+        };
+
+        this.postData = function(route,postData){
+            console.log("POST data");
+            console.log(connectInfo.url + ":" + connectInfo.port + "/" + route);
+            console.log(postData);
+
+            //Test method to POST data example.
+            $http({
                 method  : 'POST',
-                url     : 'https://httpbin.org/post',
+                url     : connectInfo.url + ":" + connectInfo.port + "/" + route,
                 data    : postData,
-                headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+                headers : { 'Content-Type': 'application/json',
+                            'Authorization' : 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjIsImlhdCI6MTQ3OTIyMjM0OX0.WAi12LVaNRQAjck2CnLDKifM1DHrTBnz9jtf6HXCel4'
+                          }
                })
                 .success(function(data) {
                   if (data.errors) {
@@ -25,5 +55,6 @@
 
             return 200;
         }
+
       });
 })();
