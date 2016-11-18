4c24c774f4e917382e2ce3be1deca20b0b3ab306
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2016-11-18 16:16:24 +0530

    get user profile response json edited

--- client/src/app/mock-data/user-data.json.js
+++ client/src/app/mock-data/user-data.json.js
@@ -1,25 +1,34 @@
 {
-    user:{  id: 2,
+    user:{  
+      id: 2,
       username:"shaedi",
-      firstName: "Shanika",
-      lastName: "Ediriweera",
+      name: "Shanika",
+      nickName: "Ediriweera",
       email:"shaedi@abc.com",
       contactNumber:"011-121313222",
-      password:"",
-      active:true},
+      address:"address",
+      birthday:"1993-06-09",
+      designation:"intern"
+    },
     playlists:[
-        {
+      {
         id: 1,
-        name: 'Playlist1' ,
-        description:"Desc",
-        videoList:[ {id: 1, title: "title1", description:"string", url:"http://somethi.com/13"},
-                    {  id: 2, title: "title2", description:"string", url:"http://somethi.com/12"},
-                    {  id: 123, title: "title3", description:"string", url:"http://somethi.com/3"} ] },
-        {
+        name: 'Playlist1',
+        description:"string",
+        videoList:[ 
+          { id: 1, title: "title1", length:"13:20", description:"string", url:"http://somethi.com/13"},
+          { id: 2, title: "title2", length:"13:20", description:"string", url:"http://somethi.com/12"},
+          { id: 123, title: "title3", length:"13:20", description:"string", url:"http://somethi.com/3"} 
+        ] 
+      },
+      {
         id: 12,
         name: 'Playlist2' ,
-        description:"Desc",
-        videoList:[ {id: 3, title: "title1", description:"string", url:"http://somethi.com/123"},
-                    {  id: 4, title: "title2", description:"string", url:"http://somethi.com/123"} ] },
+        description:"string",
+        videoList:[ 
+          { id: 3, title: "title1", length:"13:20", description:"string", url:"http://somethi.com/123"},
+          { id: 4, title: "title2", length:"13:20", description:"string", url:"http://somethi.com/123"} 
+        ] 
+      }
     ]
 }
