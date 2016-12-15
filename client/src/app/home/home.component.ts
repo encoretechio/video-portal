c8b39903d4c4fab6dfb96fa9b3ccf1ea9c4d10c4
Udara Bibile <chathuranga94@live.com>
2016-12-15 13:06:55 +0530

    logout when auth fails

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { UserData } from '../models/user-data';
-import {User} from '../models/user';
-import {Video} from '../models/video';
-import {Playlist} from '../models/playlist';
+import { User } from '../models/user';
+import { Video } from '../models/video';
+import { Playlist } from '../models/playlist';
 import { Comment } from '../models/comment';
 import { HttpService } from '../services/http.service';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { LoginService }  from '../services/login.service';
 
 import { USER_DATA } from '../mock-data/data';
 //COMMENT_DATA
@@ -25,7 +27,11 @@ export class HomeComponent implements OnInit {
   videoId: number;
   video: Video;
 
-  constructor(private httpService:HttpService, private route: ActivatedRoute) { }
+  constructor(
+    private httpService:HttpService, 
+    private route: ActivatedRoute, 
+    private router:Router,
+    private loginService:LoginService) { }
 
   ngOnInit() {
 
@@ -42,27 +48,33 @@ export class HomeComponent implements OnInit {
     {
       this.userData = USER_DATA;
       this.httpService.getObject<User>("currentuser").subscribe(user=>{
-        this.httpService.getObject<UserData>("userprofile/"+user.id).subscribe(result=>{
-          this.userData = result;
-          console.log(this.userData);
-          //find the video object from video id;
+        this.httpService.getObject<UserData>("userprofile/"+user.id).subscribe( result=>{
+              this.userData = result;
+              console.log(this.userData);
+              //find the video object from video id;
 
-          /*
-          if(this.videoId)
-            for(let playList of this.userData.playlists){
-              console.log(playList)
-              for(let video of playList.videos){
-                console.log(video)
-                if(video.id==this.videoId)
-                {
-                  this.video = video
-                  break;
+              /*
+              if(this.videoId)
+                for(let playList of this.userData.playlists){
+                  console.log(playList)
+                  for(let video of playList.videos){
+                    console.log(video)
+                    if(video.id==this.videoId)
+                    {
+                      this.video = video
+                      break;
+                    }
+                  }
                 }
-              }
-            }
-          */
+              */
 
-        });
+          });
+      },
+      err => {
+        console.log("ERROR GETTING DATA: AUTHENTICATION ERROR  -->");
+        //this.loginService.logout(true);
+        //this.router.navigate((['login']));
+        this.loginService.logout( () =>this.router.navigate(['login']) );
       });
     }
   }
