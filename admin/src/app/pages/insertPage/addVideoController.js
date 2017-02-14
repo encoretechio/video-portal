1639f7b7b1d50419bce7ef600883d324c9348483
pranidhith <pranidhith@gmail.com>
2017-02-14 10:22:40 +0530

    video uploading added: not completed

--- admin/src/app/pages/insertPage/addVideoController.js
+++ admin/src/app/pages/insertPage/addVideoController.js
@@ -14,6 +14,15 @@ function addVideoCtrl(insertService,$scope) {
 
     $scope.initialize();
 
+    AWS.config.update({
+        accessKeyId: "AKIAI3EZYDVBHIAARKBQ",
+        secretAccessKey: "HrFACNgvTYQabH1IJ4QKhvSFN02pykOs/zfBXnQj",
+        region: "ap-south-1"  // <- If you want send something to your bucket, you need take off this settings, because the S3 are global.
+    });
+
+    var bucket = new AWS.S3({params: {Bucket: 'letsbuild-videos'}});
+
+
     $scope.submit = function(){
         if($scope.formData.title) {
             insertService.addVideo($scope.formData);
@@ -21,22 +30,32 @@ function addVideoCtrl(insertService,$scope) {
             insertService.alertMsg = "You successfully added a Video";
             insertService.showAlert();
         }
+        //var file = uploadFile.files[0];
+        if ($scope.file) {
+            var file = $scope.file;
+            var params = {Key: file.name, ContentType: file.type, Body: file};
+            bucket.upload(params, function (err, data) {
+                if(err){
+                    console.log('error');
+                } else{
+                    console.log('Uploaded');
+                }
+            });
+        } else {
+            console.log('Nothing to upload.');
+        }
     };
 
     $scope.uploadVideo = function () {
         var fileInput = document.getElementById('uploadFile');
         fileInput.click();
         console.log(fileInput.value);
-
     };
 
     $scope.getFile = function() {
-        var fileReader = new FileReader();
+        //var fileReader = new FileReader();
         $scope.file  = document.querySelector('input[type=file]').files[0];
-        fileReader.readAsDataURL($scope.file, $scope)
-            .then(function(result) {
-                $scope.images.push(result);
-            });
+        console.log($scope.file.value);
     };
 
     insertService.getPlaylists().then(function(data) {
